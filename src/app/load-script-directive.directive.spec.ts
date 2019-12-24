7f97325c229478ec8216ec90fa1b14f16ad72bc2
swissglider <npm@mailschweiz.com>
2019-12-24 15:53:25 +0100

    init

--- src/app/load-script-directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LoadScriptDirectiveDirective } from './load-script-directive.directive';
-
-describe('LoadScriptDirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LoadScriptDirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
